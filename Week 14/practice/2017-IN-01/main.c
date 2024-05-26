939ed9f3ff6139b40e757c18ee958a85f99dab21
Raya <raiagrigorova@gmail.com>
2024-05-26 03:28:53 +0300

    Add solution to 2017-IN-01

--- Week 14/practice/2017-IN-01/main.c
+++ Week 14/practice/2017-IN-01/main.c
@@ -3,50 +3,90 @@
 #include <unistd.h>
 #include <err.h>
 
+const int COMMANDS_COUNT= 5;
+const int PIPES_COUNT = 4;
+
 int main(void) {
 	// The sevents column of /etc/passwd contains the command interpreters 
 	// The bash command that I should execute is the following: 
 	// cat /etc/passwd | cut -d ':' -f 7 | sort | uniq -c | sort -n
-	
-	 const char* argv1[] = {"cat", "/etc/passwd", NULL };
-	 const char* argv2[] = {"cut", "-d", ":", "-f", "7", NULL };
-	 const char* argv3[] = {"sort", NULL };
-	 const char* argv4[] = {"uniq", "-c", NULL };
-	 const char* argv5[] = {"sort", "-n", NULL };
 
-	 const char** argument_arrays[] = {argv1, argv2, argv3, argv4 };
-  	printf("%s\n", argv1[1]);
-  	printf("%s\n", argv2[1]);
-  	printf("%s\n", argv3[0]);
-  	printf("%s\n", argv4[1]);
-  	printf("%s\n", argv5[1]);
-  	printf("%s\n", argument_arrays[1][2]);
+	// rest in pepperoni
+	// 	const char* argv1[] = {"cat", "/etc/passwd", NULL };
+	// 	const char* argv2[] = {"cut", "-d", ":", "-f", "7", NULL };
+	// 	const char* argv3[] = {"sort", NULL };
+	// 	const char* argv4[] = {"uniq", "-c", NULL };
+	// 	const char* argv5[] = {"sort", "-n", NULL };
+	// 	const char** argument_arrays[] = {argv1, argv2, argv3, argv4 };
 
-	int pipefd[4][2];
-	for(int i = 0; i < 5; i++) {
-		if (pipe(pipefd[i]) < 0) { err(3, "pipe"); }
+	// Create 4 pipes
+	// for i = 0 to 5
+	// if first command, only point stdout 
+	// if last command, only point stdin
+	// close all pipe ends that the current command doesn't need 
+	// execute the command (using a switch case and execlp)
 
-		pid_t pid = fork();
-		if (pid < 0) { err(4, "fork()"); } 
-		if (pid == 0) {
-			if (i != 0) {
-				// Not the first command, so read from a pipe
-				dup2(pipefd[i-1][0], 0);
-			}
-			if (i != 4) {
-				// Not the last command, so point stdout to the pipe
-				dup2(pipefd[i-1][1], 1);
-			}
-			// Doesn't work :(
-			// execvp(argument_arrays[i][0], argument_arrays[i]);
-			switch(i) {
-				case 0: 
-					execvp(argv1[0], argv1);
-					err(5, "exec");
-					// ...
-			}
-			err(5, "exec");
-		}
-		wait(NULL);
+	// in the parent: close all pipe ends (since the parent doesn't need them) 
+	// wait for all 5 children
+	int pipefd[PIPES_COUNT][2];
+	for(int i = 0; i < PIPES_COUNT; i++) {
+		if (pipe(pipefd[i]) < 0) { err(1, "pipe"); }
 	}
+
+	for (int i = 0; i < COMMANDS_COUNT; i++) {
+		pid_t pid = fork();
+		if (pid < 0) { err(4, "fork"); }
+
+		if (pid == 0) {
+			int pipe_read = i - 1;
+			int pipe_write = i;
+
+			for (int j = 0; j < PIPES_COUNT; j++) {
+				if (j != pipe_read && j != pipe_write) {
+					close(pipefd[j][0]);
+					close(pipefd[j][1]);
+				}
+			}
+			if (i != 0) {
+				close(pipefd[pipe_read][1]);
+				dup2(pipefd[pipe_read][0], 0);
+				close(pipefd[pipe_read][0]);
+			}
+			if (i < PIPES_COUNT) {
+				close(pipefd[pipe_write][0]);
+				dup2(pipefd[pipe_write][1], 1);
+				close(pipefd[pipe_write][1]);
+			}
+			
+			switch(i){
+				case 0:
+					execlp("cat", "cat", "/etc/passwd", (char*) NULL);
+					err(2, "exec");
+				case 1:
+					execlp("cut", "cut", "-d", ":", "-f", "7", (char*) NULL);
+					err(2, "exec");
+				case 2:
+					execlp("sort", "sort", (char*) NULL);
+					err(2, "exec");
+				case 3:
+					execlp("uniq", "uniq", "-c", (char*) NULL);
+					err(2, "exec");
+				case 4:
+					execlp("sort", "sort", "-n", (char*) NULL);
+					err(2, "exec");
+				default:
+					errx(3, "execlp switch case");
+			}
+		}
+	}
+
+	for (int i = 0; i < PIPES_COUNT; i++) {
+		close(pipefd[i][0]);
+		close(pipefd[i][1]);
+	}
+  	for (int i = 0; i < COMMANDS_COUNT; i++) {
+  		int status;
+  		if (wait(&status) < 0) { err(6, "wait"); }
+  		if (!WIFEXITED(status) || WEXITSTATUS(status)) { warnx("Child didn't exit properly"); }
+  	}
 }
