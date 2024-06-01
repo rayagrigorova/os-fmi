a1d66d24561231de374d002dd1a2f010ae1165a6
Raya <raiagrigorova@gmail.com>
2024-06-01 10:33:51 +0300

    Fix 2020-IN-01

--- Week 15/practice/2020-IN-01/main.c
+++ Week 15/practice/2020-IN-01/main.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
@@ -6,19 +7,21 @@
 
 struct header {
 	uint32_t magic; 
-	uint8_t header_version;
+	uint8_t header_version; // 0x01
 	uint8_t data_version;
 	uint16_t count; 
 	uint32_t reserved_1;
 	uint32_t reserved_2;
 }__attribute__((packed));
 
+// when data version is 0x00
 struct structure_1_record {
 	uint16_t offset;
 	uint8_t original_byte;
 	uint8_t new_byte;
 }__attribute__((packed));
 
+// when data version is 0x01
 struct structure_2_record {
 	uint32_t offset;
 	uint16_t original_word;
@@ -40,6 +43,7 @@ void copy_file(int src, int dest) {
 		err(6, "read");
 	}
 
+	// Return to the beginnings of both files
 	if (lseek(src, 0, SEEK_SET) < 0) {
 		err(5, "lseek");
 	}
@@ -59,7 +63,8 @@ void change_bytes(int fd_patch, int fd1, int fd2) {
 	// in relation to the file size, return an error 
 	// Else, lseek in f1. If the atual original byte 
 	// doesn't match the expected one, errx()
-	// Else, write over it with new_byte
+	// Else, lseek in f2 to the same 
+	// position and write over it with new_byte
 	uint8_t f1_buff;
 	struct structure_1_record record;
 	int read_res;
@@ -68,7 +73,7 @@ void change_bytes(int fd_patch, int fd1, int fd2) {
 	if (fstat(fd1, &statbuf) < 0) { err(15, "fstat"); }
 	off_t f1_size = statbuf.st_size;
 
-	while((read_res = read(fd_patch, &record, sizeof(struct structure_1_record))) < 0) {
+	while((read_res = read(fd_patch, &record, sizeof(struct structure_1_record))) > 0) {
 		if(record.offset >= f1_size) {
 			errx(17, "Invalid offset");
 		}
@@ -81,8 +86,7 @@ void change_bytes(int fd_patch, int fd1, int fd2) {
 		if (f1_buff != record.original_byte) {
 			errx(22, "The original byte doesn't exist in the file");
 		}
-		// Now, lseek one positionback and then write 
-		if (lseek(fd1, -1, SEEK_CUR) < 0) {
+		if (lseek(fd2, record.offset, SEEK_SET) < 0) {
 			err(23, "lseek 2"); 
 		}
 		if (write(fd2, &record.new_byte, sizeof(record.new_byte)) < 0) {
@@ -102,21 +106,20 @@ void change_words(int fd_patch, int fd1, int fd2) {
 	if (fstat(fd1, &statbuf) < 0) { err(15, "fstat"); }
 	off_t f1_size = statbuf.st_size;
 
-	while((read_res = read(fd_patch, &record, sizeof(struct structure_2_record))) < 0) {
-		if(record.offset >= f1_size) {
+	while((read_res = read(fd_patch, &record, sizeof(struct structure_2_record))) > 0) {
+		if(record.offset * 2 >= f1_size) { 
 			errx(17, "Invalid offset");
 		}
-		if (lseek(fd1, record.offset, SEEK_SET) < 0) {
+		if (lseek(fd1, record.offset * 2, SEEK_SET) < 0) {
 			err(20, "lseek");
 		}
 		if (read(fd1, &f1_buff, sizeof(f1_buff)) < 0) {
 			err(21, "read in buff");
 		}
 		if (f1_buff != record.original_word) {
-			errx(22, "The original byte doesn't exist in the file");
+			errx(22, "The original word doesn't exist in the file");
 		}
-		// Now, lseek one positionback and then write 
-		if (lseek(fd1, -1, SEEK_CUR) < 0) {
+		if (lseek(fd2, record.offset * 2, SEEK_SET) < 0) {
 			err(23, "lseek 2"); 
 		}
 		if (write(fd2, &record.new_word, sizeof(record.new_word)) < 0) {
@@ -133,11 +136,13 @@ int main(int argc, char* argv[]) {
 
 	// Validate the patch.bin file: 
 	// Check if its size is at least 16
+	// (since the header has a size of 16)
 	// If yes, read the header 
 	// If data version == 0, then the file size 
-	// Should be 16 + count * 4
+	// should be 16 + count * 4 (because each entry in the data section is 4 bytes)
 	// If data version == 1, then the file size 
 	// should be 16 + count * 8
+	// (because each entry in the data section is 4 bytes)
 
 	struct stat statbuf;
 	if (stat(argv[1], &statbuf) < 0) { err(2, "stat"); }
@@ -147,6 +152,7 @@ int main(int argc, char* argv[]) {
 	if(fd_patch < 0) { err(4, "open"); } 
 
 	struct header patch_file_header;
+	// Read the header
 	if(read(fd_patch, &patch_file_header, sizeof(struct header)) < 0) {
 		err(5, "read"); 
 	}
@@ -159,36 +165,38 @@ int main(int argc, char* argv[]) {
 		errx(3, "Invalid patch file format: values in header are wrong\n");
 	}
 
+	// Bytes 
 	if (patch_file_header.data_version == 0x00) {
 		if (statbuf.st_size != 16 + patch_file_header.count * 4) {
 			errx(3, "Invalid patch file format: the data version is 0x00, total patch file size doesn't correspond\n"); 
 		}
 	}
-	else {
+
+	// Words
+	else { 
 		if (statbuf.st_size != 16 + patch_file_header.count * 8) {
 			errx(3, "Invalid patch file format: the data version is 0x01, total patch file size doesn't correspond\n"); 
 		}
+		// Also check the size of f1 according to the version:
+		// If the version is 1, then the file size should 
+		// be divisible by 2
+		if (stat(argv[2], &statbuf) < 0) { 
+			err(2, "stat");
+		}
+		if (statbuf.st_size % 2) {
+			errx(3, "Invalid patch file format: data version is 0x01 and the size of f1 is odd\n"); 
+		}
 	}
 
-	// Also check the size of f1 according to the version:
-	// If the version is 1, then the file size should 
-	// be divisible by 2
-	if (stat(argv[2], &statbuf) < 0) { 
-		err(2, "stat");
-	}
-	if (patch_file_header.data_version == 0x01 && statbuf.st_size % 2) {
-		errx(3, "Invalid patch file format: data version is 0x01 and the size of f1 is odd\n"); 
-	}
-
+	// Actual logic of the program
 	int f1 = open(argv[2], O_RDONLY);
 	if(f1 < 0) { err(4, "open"); }
-	int f2 = open(argv[3], O_WRONLY | O_CREAT, 0666);
+	int f2 = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if(f2 < 0) { err(4, "open"); }
 
 	// Alogirthm: copy f1 into f2 
 	// For each entry from the patch file: 
 	// Based on the data version, change words or bytes (but first check the offset of course) :D
-	// The offset should be scaled in accordance with the data version as well
 	// Also, check if the original byte/word is actually present in f1
 	copy_file(f1, f2);
 	if (patch_file_header.data_version == 0x00) {
@@ -200,4 +208,5 @@ int main(int argc, char* argv[]) {
 
 	close(fd_patch);
 	close(f1);
+	close(f2);
 }
